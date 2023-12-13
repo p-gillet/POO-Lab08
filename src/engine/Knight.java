6c3805772400f0c5f56a1882fbbe0fce682ad288
Paul Gillet <paul3562@live.fr>
2023-12-13 13:45:43 +0100

    Update Knight.java

--- src/engine/Knight.java
+++ src/engine/Knight.java
@@ -4,12 +4,17 @@ import chess.PieceType;
 import chess.PlayerColor;
 
 public class Knight extends Piece {
-    public Knight(PlayerColor color, Square square){
-        super(PieceType.KNIGHT, color, square);
+    public Knight(PlayerColor color, Square square, Board board){
+        super(color, square, board);
     }
 
     @Override
     public boolean canMove(Square dest) {
         return false;
+    }
+
+    @Override
+    public PieceType getType() {
+        return PieceType.KNIGHT;
     }
 }
