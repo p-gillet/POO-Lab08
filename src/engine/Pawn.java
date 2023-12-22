c470beeeddba61bf04dbf14bb1496676ed7e8860
p-gillet <paul3562@live.fr>
2023-12-22 11:34:17 +0100

    Update Pawn.java

--- src/engine/Pawn.java
+++ src/engine/Pawn.java
@@ -4,8 +4,12 @@ import chess.PieceType;
 import chess.PlayerColor;
 
 public class Pawn extends Piece implements LinearMove {
-    public Pawn(PlayerColor color, Square square, Board board) {
+    private final int goesUp; // 1 si le pion va vers le haut, -1 s'il va vers le bas
+    private boolean enPassantVictim; // true si le pion est une victime de la prise en passant
+    public Pawn(PlayerColor color, Square square, Board board){
         super(color, square, board);
+        goesUp = color == PlayerColor.WHITE ? 1 : -1;
+        enPassantVictim = false;
     }
 
     @Override
