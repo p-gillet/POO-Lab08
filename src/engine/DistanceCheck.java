0be55e257b177fb7a12abf7a30dce342c078ea26
Artotrogo <louis.haye@heig-vd.ch>
2024-01-02 11:05:14 +0100

    update comms

--- src/engine/DistanceCheck.java
+++ src/engine/DistanceCheck.java
@@ -5,13 +5,14 @@ import java.awt.*;
 public interface DistanceCheck {
 
     /**
-     * Méthode qui retourne une case qui a comme coordonnée :
+     * Méthode qui retourne un point qui a comme coordonnée :
      * - x qui est égal à la valeur absolue de la différence entre la coordonnée x
      *   de la case actuelle et celle de la case cible
      * - y qui est égal à la valeur absolue de la différence entre la coordonnée y
      *   de la case actuelle et celle de la case cible
-     * @param to case cible
-     * @return la case nouvellement créée
+     * @param from la case actuelle
+     * @param to la case cible
+     * @return un Point nouvellement créé
      */
     default Point getDistance(Square from, Square to){
         int x = from.getX() - to.getX();
@@ -20,11 +21,12 @@ public interface DistanceCheck {
     }
 
     /**
-     * Méthode qui retourne une case qui a comme coordonnée :
+     * Méthode qui retourne un point qui a comme coordonnée :
      * - x qui est égal à la différence entre la coordonnée x de la case cible et celle de la case actuelle
      * - y qui est égal à la différence entre la coordonnée y de la case cible et celle de la case actuelle
-     * @param to case cible
-     * @return la case nouvellement créée
+     * @param from la case actuelle
+     * @param to la case cible
+     * @return un Point nouvellement créé
      */
     default Point getTrueDistance(Square from, Square to) {
         int x = to.getX() - from.getX();
